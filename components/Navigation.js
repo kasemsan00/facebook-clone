259abb9fc9a519de03251a1e147a28484dd76364
kasemsan00 <kasemsan.cho@gmail.com>
2022-08-03 22:05:21 +0700

    init

---
+++ components/Navigation.js
@@ -0,0 +1,96 @@
+import Image from 'next/image'
+
+export default function Navigation () {
+    return (
+        <>
+            <div className='flex flex-1 justify-center drop-shadow-sm bg-white ' >
+                <div className='flex self-center w-auto cursor-pointer '>
+                    <Image width={40} height={40} src={require('../assets/facebook-logo.png')} />
+                </div>
+                <input 
+                    className='w-6/12 text-base p-2 px-2 rounded-full border-ra m-2 bg-gray-100'
+                    type='text' 
+                    placeholder='Search Facebook'
+                />
+            </div>
+            <div className='flex-1'>
+                <ul> 
+                    <li className='flex flex-1 m-2 pl-2 pr-2 rounded-lg mgin hover:bg-gray-200 cursor-pointer '>
+                        <div className='flex ml-2 mr-2 mt-2 mb-2 self-center'>
+                            <Image 
+                                width={30}
+                                height={30}
+                                src={require('../assets/user.png')} 
+                            />
+                        </div>
+                        <div className='flex flex-1 self-center font-medium text-sm'>
+                            Kasemsan Chompuwised
+                        </div>
+                    </li>
+                    <li className='flex flex-1 m-2 pl-2 pr-2 rounded-lg mgin hover:bg-gray-200 cursor-pointer '>
+                        <div className='flex ml-2 mr-2 mt-2 mb-2 self-center'>
+                            <Image 
+                                width={30}
+                                height={30}
+                                src={require('../assets/friends.png')} 
+                            />
+                        </div>
+                        <div className='flex flex-1 self-center font-medium text-sm'>
+                            Friends
+                        </div>
+                    </li>
+                    <li className='flex flex-1 m-2 pl-2 pr-2 rounded-lg mgin hover:bg-gray-200 cursor-pointer '>
+                        <div className='flex ml-2 mr-2 mt-2 mb-2 self-center'>
+                            <Image 
+                                width={30}
+                                height={30}
+                                src={require('../assets/connection.png')} 
+                            />
+                        </div>
+                        <div className='flex flex-1 self-center font-medium text-sm'>
+                            Groups
+                        </div>
+                    </li>
+                    <li className='flex flex-1 m-2 pl-2 pr-2 rounded-lg mgin hover:bg-gray-200 cursor-pointer '>
+                        <div className='flex ml-2 mr-2 mt-2 mb-2 self-center'>
+                            <Image 
+                                width={30}
+                                height={30}
+                                src={require('../assets/store.png')} 
+                            />
+                        </div>
+                        <div className='flex flex-1 self-center font-medium text-sm'>
+                            Marketplace
+                        </div>
+                    </li>
+                    <li className='flex flex-1 m-2 pl-2 pr-2 rounded-lg mgin hover:bg-gray-200 cursor-pointer '>
+                        <div className='flex ml-2 mr-2 mt-2 mb-2 self-center'>
+                            <Image 
+                                width={30}
+                                height={30}
+                                src={require('../assets/video-marketing.png')} 
+                            />
+                        </div>
+                        <div className='flex flex-1 self-center font-medium text-sm'>
+                            Watch
+                        </div>
+                    </li>
+                    <li className='flex flex-1 m-2 pl-2 pr-2 rounded-lg mgin hover:bg-gray-200 cursor-pointer '>
+                        <div className='flex ml-2 mr-2 mt-2 mb-2 self-center'>
+                            <Image 
+                                width={30}
+                                height={30}
+                                src={require('../assets/more-or-less.png')} 
+                            />
+                        </div>
+                        <div className='flex flex-1 self-center font-medium text-sm'>
+                            See more
+                        </div>
+                    </li>
+                </ul>
+            </div>
+        </>
+        
+
+    )
+}
---
+++ pages/index.js
@@ -0,0 +1,28 @@
+import Head from 'next/head'
+import Content from '../components/Content'
+import Navigation from '../components/Navigation'
+import Complementary from '../components/Complementary'
+
+export default function Home() {
+	return (
+		<>
+			<div className='flex flex-1' >
+				<div className='flex flex-1 flex-col bg-gray-100 '>
+					<Navigation/>
+				</div>
+				<div className='flex flex-1 flex-col bg-red-300'>
+					<Content/>
+				</div>
+				<div className='flex flex-1 bg-yellow-50'>
+					<Complementary/>
+				</div>
+			</div>
+
+			{/* <div className='flex flex-row drop-shadow-md bg-gray-400'>
+
+				
+
+			</div> */}
+		</>
+	)
+}
